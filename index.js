d1c6aa916f5682432ad377b7daee8ed611c8eda8
MrSohailAhmad <ahmadsohail1819@gmail.com>
2025-06-03 04:31:58 +0500

    update routes

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.get("", (req, res) => {
   res.status(200).json({ message: "Home Route" });
 });
 
+app.get("/about", (req, res) => {
+  res.status(200).json({ message: "About Route" });
+});
+
+app.get("/contact", (req, res) => {
+  res.status(200).json({ message: "Contact Route" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server are running now on : http://localhost:${PORT}`);
 });
