333ab5eea04ca8e7003d3f831e86c753325cee63
MrSohailAhmad <ahmadsohail1819@gmail.com>
2025-06-03 05:14:33 +0500

    add more routes

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ app.get("/contact", (req, res) => {
   res.status(200).json({ message: "Contact Route" });
 });
 
+app.get("/services", (req, res) => {
+  res.status(200).json({ message: "Services Route" });
+});
+
+app.get("/products", (req, res) => {
+  res.status(200).json({ message: "Products Route" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server are running now on : http://localhost:${PORT}`);
 });
